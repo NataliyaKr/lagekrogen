0b4033a74c0cdec156a88edd625003ea05f36b54
Nataliya Krasyuk <116311212+NataliyaKr@users.noreply.github.com>
2025-04-01 09:57:02 +0200

    test up

--- src/components/accordions/accordions.jsx
+++ src/components/accordions/accordions.jsx
@@ -7,13 +7,13 @@ const Accordion = ({ q }) => {
 
   const toggleMenuPanel = () => {
     setIsOpenPanel(!isOpenPanel);
-  };
+  }; //En funksjon som inverterer verdien av isOpenPanel. Denne brukes som en event-handler for klikk-hendelser.
   return (
     <figure className={styles.question} key={q._id}>
       <p className={styles.acText}>{q.question}</p>
       <GoChevronDown
         size={37}
-        className={styles.qIcon}
+        className={styles.qIcon}sed
         onClick={toggleMenuPanel}
       />
       <div>{isOpenPanel && <p className={styles.panel}>{q.answer}</p>}</div>
